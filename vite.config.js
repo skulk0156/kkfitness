a26a5682c512a32ec4729924cde9a289b77efc9a
Shubham Kulkarni <skulk156.pc@gmail.com>
2025-12-28 00:16:38 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kkfitness/'   // 👈 repo name
+  base: './'  // Important! Makes paths relative
 })
